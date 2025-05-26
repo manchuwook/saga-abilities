ec0772ee2d94db3eed3bfe70643c0e3cd084c8c4
Steven Altsman <manchuwook@tinhalo.com>
2025-05-25 20:12:20 -0500

    chore: update pnpm workspace and Vite configuration
    - Added 'sharp' and 'workerd' to onlyBuiltDependencies in pnpm-workspace.yaml
    - Updated Vite configuration to include Cloudflare plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { VitePWA } from 'vite-plugin-pwa';
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -35,6 +36,7 @@ export default defineConfig({
     }),
     nxViteTsPaths(),
     nxCopyAssetsPlugin(['*.md']),
+    cloudflare()
   ],
   resolve: {
     alias: {
